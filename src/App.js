800e71cb517be2c49a7da0e927ba6d49c9732161
Azarul Islam <azarul81@gmail.com>
2023-11-12 19:09:14 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -27,17 +27,17 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
+              <Route path="my-dashboard/" element={<Dashboard />} />
+              <Route path="my-dashboard/team" element={<Team />} />
+              <Route path="my-dashboard/contacts" element={<Contacts />} />
+              <Route path="my-dashboard/invoices" element={<Invoices />} />
+              <Route path="my-dashboard/form" element={<Form />} />
+              <Route path="my-dashboard/bar" element={<Bar />} />
+              <Route path="my-dashboard/pie" element={<Pie />} />
+              <Route path="my-dashboard/line" element={<Line />} />
+              <Route path="my-dashboard/faq" element={<FAQ />} />
+              <Route path="my-dashboard/calendar" element={<Calendar />} />
+              <Route path="my-dashboard/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
